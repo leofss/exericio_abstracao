20dae23a8be44ad65cc2efa9a8f46b20d419b347
leonardo fernandes <leofernandess11@gmail.com>
2022-02-21 14:41:42 -0300

    Inserido lógica de inout do usuário

---
+++ Entities/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using exericio_abstração.Entities.Enums;
+
+namespace exericio_abstração.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using exericio_abstração.Entities.Enums;
+using exericio_abstração.Entities;
 
 namespace exericio_abstração
 {
@@ -6,7 +9,40 @@ namespace exericio_abstração
     {
         static void Main(string[] args)
         {
-            
+            List<Shape> list = new List<Shape>();
+
+            Console.WriteLine("Entre com a quantidade de figuras: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Figura numero {i}");
+                Console.WriteLine("Retangulo ou circulo r/c ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Qual a cor (black, blue, red)");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+                if(ch == 'r')
+                {
+                    Console.WriteLine("Altura: ");
+                    double alt = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Largura: ");
+                    double lg = double.Parse(Console.ReadLine());
+                    list.Add(new Rectangle(lg, alt, color));
+                }
+                else if(ch == 'c')
+                {
+                    Console.WriteLine("Radius: ");
+                    double rad = double.Parse(Console.ReadLine());
+                    list.Add(new Circle(rad, color));
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Area das figuras");
+            foreach(Shape shape in list)
+            {
+                Console.WriteLine(shape.Area());
+            }
         }
     }
 }
